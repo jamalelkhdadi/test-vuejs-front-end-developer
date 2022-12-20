faeb14f0337b35940ae615ddee5fdbe9cbb1edae
jamalelkhdadi <jamal.elkhdadi@outlook.com>
2022-12-20 22:40:46 +0100

    jsja

--- vue-crud/src/router.js
+++ vue-crud/src/router.js
@@ -1,8 +1,24 @@
 import { createWebHistory, createRouter } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+
+
 
 const routes =  [
   {
-    path: "/",
+      path: "/",
+      name: "home",
+      component: HomeView
+    },
+    {
+      path: "/about",
+      name: "about",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/AboutView.vue")
+    },
+  {
+    path: "/tutorials",
     alias: "/tutorials",
     name: "tutorials",
     component: () => import("./components/TutorialsList")
